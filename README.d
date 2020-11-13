e0fc0687284e30eb265282caae78e1758220aa01
ugnelink <71488162+ugnelink@users.noreply.github.com>
2020-11-13 13:08:37 +0200

    Create README.d

---
+++ README.d
@@ -0,0 +1,26 @@
+# laboratorinis
+# v.05 versija
+studentai1000.txt:
+
+    Nuskaitymas: 11.118 s
+    Suskirstymas: 0.0276316 s
+
+studentai10000.txt:
+
+    Nuskaitymas: 14.847 s
+    Suskirstymas: 0.331925 s
+    
+studentai100000.txt:
+
+    Nuskaitymas: 35.7107s
+    Suskirstymas: 3.0638s
+studentai1000000.txt:
+
+    Nuskaitymas: 155.115 s
+    Suskirstymas: 29.6535 s
+
+studentai10000000.txt:
+
+    Nuskaitymas: s
+    Suskirstymas: s
+
